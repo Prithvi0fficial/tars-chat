2e4b6dd133d09629f180eb10dc015c02c3f1542e
Prithvi <312walkerofficial.com@gmail.com>
2026-02-26 13:49:29 +0530

    feat: Added sidebar serach

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,13 +4,13 @@ import { useUser, UserButton } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 import ConversationItem from "./ConversationItem";
-
+import { useState } from "react";
 export default function Sidebar() {
 
   const { user } = useUser();
 
   const users = useQuery(api.users.getUsers);
-
+  const [search, setSearch] = useState("");
   const currentUser =
     users?.find(u => u.clerkId === user?.id);
 
@@ -47,20 +47,51 @@ export default function Sidebar() {
         <UserButton />
 
       </div>
+      <div style={{ padding: 9 }}>
 
-      {/* Conversations */}
-
-      {conversations.map(conversation => (
-
-        <ConversationItem
-          key={conversation._id}
-          conversation={conversation}
-          currentUser={currentUser}
-          users={users}
+        <input
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          style={{
+            width: "95%",
+            padding: 8,
+            borderRadius: 6,
+            border: "1px solid #ddd"
+          }}
         />
 
-      ))}
+      </div>
+      {/* Conversations */}
 
+      {conversations
+
+        .filter(conversation => {
+
+          const otherUser =
+            users.find(
+              u =>
+                conversation.memberIds.includes(u._id)
+                &&
+                u._id !== currentUser._id
+            );
+
+          return otherUser?.name
+            .toLowerCase()
+            .includes(search.toLowerCase());
+
+        })
+
+        .map(conversation => (
+
+          <ConversationItem
+            key={conversation._id}
+            conversation={conversation}
+            currentUser={currentUser}
+            users={users}
+          />
+
+        ))}
     </div>
 
   );
