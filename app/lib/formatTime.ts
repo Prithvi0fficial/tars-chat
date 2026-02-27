47c411c5ea0d43e24d182b7cdf387bc0691507fe
Prithvi <312walkerofficial.com@gmail.com>
2026-02-27 16:02:19 +0530

    Update : TimeStamp updated yesterday,date, year

--- app/lib/formatTime.ts
+++ app/lib/formatTime.ts
@@ -1,34 +1,46 @@
 export function formatTime(timestamp: number) {
-  const date = new Date(timestamp);
-  const now = new Date();
+    const date = new Date(timestamp);
+    const now = new Date();
 
-  const isToday =
-    date.toDateString() === now.toDateString();
+    const isToday =
+        date.toDateString() === now.toDateString();
 
-  const isSameYear =
-    date.getFullYear() === now.getFullYear();
+    const isSameYear =
+        date.getFullYear() === now.getFullYear();
+    const yesterday = new Date();
+    yesterday.setDate(now.getDate() - 1);
 
-  if (isToday) {
-    return date.toLocaleTimeString([], {
-      hour: "2-digit",
-      minute: "2-digit",
-    });
-  }
+    const isYesterday =
+        date.toDateString() === yesterday.toDateString();
 
-  if (isSameYear) {
+    if (isYesterday) {
+        return `Yesterday, ${date.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+        })}`;
+    }
+
+    if (isToday) {
+        return date.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    }
+
+    if (isSameYear) {
+        return date.toLocaleString([], {
+            month: "short",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+        });
+    }
+
     return date.toLocaleString([], {
-      month: "short",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
     });
-  }
-
-  return date.toLocaleString([], {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-  });
 }
